0fd3f0a0f0d8f2a6ee0b5f7b5e13b16015ade716
Ignacio Díaz <ignacio.db@icodesx.com>
2023-12-15 19:25:22 -0300

    Added types

---
+++ types.d.ts
@@ -0,0 +1,140 @@
+type ApiActivitiesList = {
+  data: SummaryActivity[];
+};
+
+type ApiMonthlyStats = {
+  data: MonthlyStats[];
+};
+
+type LatLng = [number, number];
+
+type MetaAthlete = {
+  id: number;
+};
+
+type MonthlyStats = {
+  id: number;
+  month_id: number;
+  month_name: string;
+  total_distance: number;
+  total_time: number;
+  total_elevation_gain: number;
+};
+
+type PolylineMap = {
+  id: string;
+  polyline: string;
+  summary_polyline: string;
+};
+
+type SportType =
+  | 'AlpineSki'
+  | 'BackcountrySki'
+  | 'Badminton'
+  | 'Canoeing'
+  | 'Crossfit'
+  | 'EBikeRide'
+  | 'Elliptical'
+  | 'EMountainBikeRide'
+  | 'Golf'
+  | 'GravelRide'
+  | 'Handcycle'
+  | 'HighIntensityIntervalTraining'
+  | 'Hike'
+  | 'IceSkate'
+  | 'InlineSkate'
+  | 'Kayaking'
+  | 'Kitesurf'
+  | 'MountainBikeRide'
+  | 'NordicSki'
+  | 'Pickleball'
+  | 'Pilates'
+  | 'Racquetball'
+  | 'Ride'
+  | 'RockClimbing'
+  | 'RollerSki'
+  | 'Rowing'
+  | 'Run'
+  | 'Sail'
+  | 'Skateboard'
+  | 'Snowboard'
+  | 'Snowshoe'
+  | 'Soccer'
+  | 'Squash'
+  | 'StairStepper'
+  | 'StandUpPaddling'
+  | 'Surfing'
+  | 'Swim'
+  | 'TableTennis'
+  | 'Tennis'
+  | 'TrailRun'
+  | 'Velomobile'
+  | 'VirtualRide'
+  | 'VirtualRow'
+  | 'VirtualRun'
+  | 'Walk'
+  | 'WeightTraining'
+  | 'Wheelchair'
+  | 'Windsurf'
+  | 'Workout'
+  | 'Yoga';
+
+type SummaryActivity = {
+  resource_state: number;
+  athlete: {
+    id: number;
+    resource_state: number;
+  };
+  name: string;
+  distance: number;
+  moving_time: number;
+  elapsed_time: number;
+  total_elevation_gain: number;
+  type: string;
+  sport_type: string | null;
+  workout_type: string | null;
+  id: number;
+  external_id: string;
+  upload_id: number;
+  start_date: string;
+  start_date_local: string;
+  timezone: string;
+  utc_offset: number;
+  start_latlng: LatLng | null;
+  end_latlng: LatLng | null;
+  location_city: string | null;
+  location_state: string | null;
+  location_country: string | null;
+  achievement_count: number;
+  kudos_count: number;
+  comment_count: number;
+  athlete_count: number;
+  photo_count: number;
+  map: {
+    id: string;
+    summary_polyline: null;
+    resource_state: number;
+  };
+  trainer: boolean;
+  commute: boolean;
+  manual: boolean;
+  private: boolean;
+  flagged: boolean;
+  gear_id: string;
+  from_accepted_tag: boolean;
+  average_speed: number;
+  max_speed: number;
+  average_cadence: number;
+  average_watts: number;
+  weighted_average_watts: number;
+  kilojoules: number;
+  device_watts: boolean;
+  has_heartrate: boolean;
+  average_heartrate: number;
+  max_heartrate: number;
+  max_watts: number;
+  pr_count: number;
+  total_photo_count: number;
+  has_kudoed: boolean;
+  suffer_score: number;
+};
